68e1958247d6307310449fbc15130bb75739efdf
Syed Badar Yazdan <128181533+DrakoDragneel@users.noreply.github.com>
2023-06-09 01:08:35 +0500

    footer

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,92 @@
-
+<!DOCTYPE html>
+<html>
+<?php include 'include/head.php';?>
+<body>
+<footer class="footer">
+        <div class="container">
+        <div class="row">
+            <!--Footer 1st Column-->
+        <div class="footer-col">
+        <h1 class="FH">ANEXCON</h1>
+        <ul>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Carrers</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Customers</a></li>
+        <li><a href="#">Partners</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Sign in</a></li>
+        <li><a href="#">Security</a></li>
+        </ul>
+        </div>
+        <!--Footer 2nd Column-->
+        <div class="footer-col">
+            <h1 class="FH">OUR PRODUCTS</h1>
+            <ul>
+            <li><a href="#">Employee Self Service</a></li>
+            <li><a href="#">Analytics and Reporting</a></li>
+            <li><a href="#">Time and Attendace</a></li>
+            <li><a href="#">Recruitment</a></li>
+            <li><a href="#">Payroll</a></li>
+            <li><a href="#">Management</a></li>
+            </ul>
+            </div>
+            <!--Footer 3rd Column-->
+            <div class="footer-col">
+                <h1 class="FH">SOLUTIONS</h1>
+                <ul>
+                <li><a href="#">Agencies</a></li>
+                <li><a href="#">Professional Services</a></li>
+                <li><a href="#">Technology</a></li>
+                <li><a href="#">Banks and Credit Unions</a></li>
+                <li><a href="#">Fintech</a></li>
+                <li><a href="#">Life Sciences</a></li>
+                <li><a href="#">Insurance</a></li>
+                <li><a href="#">Small businesses</a></li>
+                <li><a href="#">Enterprise</a></li>
+                <li><a href="#">Chief Executive Officers</a></li>
+                <li><a href="#">Human Resources Leaders</a></li>
+                </ul>
+                </div>
+                <!--Footer 4th Column-->
+                <div class="footer-col">
+                    <h1 class="FH">RESOURCES</h1>
+                    <ul>
+                    <li><a href="#">Livrary</a></li>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="#">Interview</a></li>
+                    <li><a href="#">Webinars</a></li>
+                    <li><a href="#">Ebooks</a></li>
+                    <li><a href="#">Podcasts</a></li>
+                    <li><a href="#">Community</a></li>
+                    <li><a href="#">Templates</a></li>
+                    </ul>
+                    </div>
+                    <!--Footer 5th Column-->
+                    <div class="footer-col">
+                        <h1 class="FH">SUPPORT</h1>
+                        <ul>
+                        <li><a href="#">Help Center</a></li>
+                        <li><a href="#">Privacy Statement</a></li>
+                        <li><a href="#">Status</a></li>
+                        <li><a href="#">Privacy Request</a></li>
+                        <li><a href="#">Privacy Opt-Out</a></li>
+                        <li><a href="#">Anexcon University</a></li>
+                        </ul>
+                        </div>
+                        <div class="footer-col">
+                            <h1 class="FH">CONTACT US</h1>
+                            <ul>
+                            <li><a href="#">SpringCity PA19475,USA</a></li>
+                            <li><a href="#">customercare@anexcon.com</a></li>
+                            </ul>
+                            </div>
+            </div>    
+            <p class="FE">
+                @ Copyright 2023, All Rights Reserved.
+            </p>       
+        </div>
+      </footer>
 <script type="text/javascript" src="assets/js/jquery-3.2.1.slim.min.js"></script>
 <script type="text/javascript" src="assets/js/popper.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
@@ -6,3 +94,5 @@
 <script type="text/javascript" src="assets/js/slider.js"></script>
 <script src="assets/js/code.jquery.com_jquery-2.2.0.min.js" type="text/javascript"></script>
 <script src="assets/js/cdnjs.cloudflare.com_ajax_libs_slick-carousel_1.6.0_slick.js"></script>
+</body>
+</html>
